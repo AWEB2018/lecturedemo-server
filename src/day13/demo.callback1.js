0a5d31cca0ce190f066482e76f1165f95c15decd
santosh <santosh.ece06@gmail.com>
2018-05-12 12:32:25 +0530

    calllback

--- src/day13/demo.callback1.js
+++ src/day13/demo.callback1.js
@@ -1,7 +1,35 @@
 class DemoCallback1{
 
-    sayHi(){
-        return "Helloo!!"
+    sayHi(name){
+        return "Helloo!! " + name;
+    }
+
+
+    callNetwork(){
+
+        setTimeout(function(){
+            console.log("Afte N-Seconds");
+            return "Hello Netowrk";
+        }, 20000)
+
+        return "Network!!"
+    }
+
+
+
+    callNetworkUsingCallback(cb){
+
+        setTimeout(function(){
+            try{
+                console.log("Afte N-Seconds");
+            
+                let output = "Hello Netwrok!!!!"
+                let output1 = 1000;
+                cb(output, output1);
+            }catch(err){
+                console.log(err);
+            }
+        }, 10000)
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -7,9 +7,16 @@ class MainProgram {
             console.log("Lets learn callback...!!!");
 
             let ref = new DemoCallabck1();
-            let res = ref.sayHi();
-            console.log(res);
-            
+
+            //let res1 = ref.callNetwork();
+            //console.log(res1);
+
+            ref.callNetworkUsingCallback();
+
+            ref.callNetworkUsingCallback(function(p1, p2){
+                console.log("I will be called; ");
+            });
+
         }catch(error){
             console.log(error);
         }
