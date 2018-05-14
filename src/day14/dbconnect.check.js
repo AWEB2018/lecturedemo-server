ab285d6f5666aab8aa1156c5c5672ff512ed97af
santosh <santosh.ece06@gmail.com>
2018-05-14 11:35:58 +0530

    update. using mysql and node

--- src/day14/dbconnect.check.js
+++ src/day14/dbconnect.check.js
@@ -1,5 +1,12 @@
 let mysql = require('mysql');
 
+let config={
+    "host":"localhost",
+    "user":"root",
+    "password":"",
+    "database":"DAC2018"
+}
+
 class DBConnectionCheck {
 
     static checkConnection(){
@@ -36,7 +43,96 @@ class DBConnectionCheck {
     }
 
 
-    
+    static readAllEmployee() {
+        try{
+            let config = {
+                "host" : "localhost",
+                "user" : "root",
+                "password" : "",
+                "database" : "DAC2018"
+            }
+
+            let connection = mysql.createConnection(config);
+            connection.connect(function(err){
+                if(err){
+                    console.log(err);
+                }
+
+                else {
+                    console.log("CONNECTION DONE!!");
+                }
+            });
+
+            let sql = "SELECT * FROM EMPLOYEE";
+            connection.query(sql, function(err, results){
+                if(err){
+                    console.log(err);
+                } else {
+                    console.log(results);
+                }
+            });
+            
+
+            // CLOSING THE CONNECTION
+            connection.end();
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+    static insertEmployee(){
+        try{
+            let config ={
+                "host":"localhost",
+                "user":"root",
+                "password":"",
+                "database":"DAC2018"
+            }
+            let connection =mysql.createConnection(config);
+
+            connection.connect(function(err){
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    console.log("connection created");
+                }
+            });
+
+            let sql="INSERT INTO EMPLOYEE (NAME,SALARY,DEPT) VALUES('Deadpool',1000000,'CEO')"
+
+            connection.query(sql, (error,data)=>{
+                if(error){
+                    console.log(error);
+                }else{
+                    console.log(data);
+                }
+
+            });
+
+            connection.end();
+
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+
+    static updateEmp(){
+        try{
+            let connection=mysql.createConnection(config);
+
+            connection.connect();
+
+            let sql="UPDATE EMPLOYEE SET NAME='SANTOSH' WHERE ID=1";
+            connection.query(sql);
+
+            connection.end();
+        }catch(err){
+            console.log(err);
+        }
+    }
 }
 
-DBConnectionCheck.checkConnection();
+DBConnectionCheck.updateEmp();
