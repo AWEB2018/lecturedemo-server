6fe8c6e67b8b48c097a3a8a2dd24e251021f3bba
santosh <santosh.ece06@gmail.com>
2018-05-14 07:46:48 +0530

    promisified.

--- src/dayx/db.main.js
+++ src/dayx/db.main.js
@@ -1,15 +1,19 @@
 let mysql = require("mysql");
 
+let Promise = require("bluebird");
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
+let config = {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "test"
+};
+
 class DBSample{
 
     static main(){
-        let config = {
-            "host": "localhost",
-            "user": "root",
-            "password": "",
-            "database": "test"
-        };
-
         let connection = mysql.createConnection(config);
 
         // STEP 3
@@ -23,6 +27,26 @@ class DBSample{
         });
         connection.end();
     }
+
+    static main1(){
+        let connection = mysql.createConnection(config);
+        
+        let promise = connection.connectAsync();
+        promise.then((data)=>{
+
+            let sql = "SELECT * FROM SAMPLE";
+            return connection.queryAsync(sql);
+        }).then((data)=>{
+
+            console.log(data);
+            return connection.endAsync();
+        }).then((data)=>{
+
+            console.log("Connection Closed!!");
+        }).catch(function(err){
+            console.log(err);
+        });
+    }
 }
 
-DBSample.main();
+DBSample.main1();
